141f4360b5049ac5f13723a8e2404da024257f3e
Paul Greetham <pgreetham@protonmail.com>
2023-04-11 14:14:51 +0200

    Update dropdown menu in nav

--- cookbook/src/app/shopping-list/shopping-list.component.ts
+++ cookbook/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -15,5 +15,9 @@ export class ShoppingListComponent {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 }
