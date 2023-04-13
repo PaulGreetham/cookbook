72ecf0c87fbe4afbd15097fcdbff13b4e8486cb4
Paul Greetham <pgreetham@protonmail.com>
2023-04-13 10:44:47 +0200

    update shopping list function

--- cookbook/src/app/header/header.component.ts
+++ cookbook/src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class HeaderComponent {
   @Output() featureSelected = new EventEmitter<string>();
+  collapsed: boolean = true;
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
